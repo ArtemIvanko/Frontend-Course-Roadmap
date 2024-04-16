4782247694d756494fae29c2f8266753606352f2
ArtemIvanko <tema30k@gmail.com>
2024-04-16 20:33:55 +0300

    Created and used getUserValue

--- Lesson 3/script.js
+++ Lesson 3/script.js
@@ -28,3 +28,15 @@ const getTypeFromValue = (value) => {
 getTypeFromValue("hello"); // string
 getTypeFromValue(123); // number
 getTypeFromValue(true); // boolean
+
+const getUserValue = () => {
+  const value1 = prompt("Enter value 1");
+  const value2 = prompt("Enter value 2");
+  const value3 = prompt("Enter value 3");
+
+  const values = [value1, value2, value3];
+
+  return console.log(values);
+};
+
+getUserValue();
