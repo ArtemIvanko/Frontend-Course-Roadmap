d49cd0bfc1b3b051baf855a09f1c170c9fbbaf2c
ArtemIvanko <tema30k@gmail.com>
2024-04-16 20:38:39 +0300

    Code cleanup

--- Lesson 3/script.js
+++ Lesson 3/script.js
@@ -1,34 +1,3 @@
-const getTypeFromValue = (value) => {
-  if (typeof value === "string") {
-    return console.log("string");
-  }
-  if (typeof value === "number") {
-    return console.log("number");
-  }
-  if (typeof value === "boolean") {
-    return console.log("boolean");
-  }
-  if (typeof value === "object") {
-    return console.log("object");
-  }
-  if (typeof value === "undefined") {
-    return console.log("undefined");
-  }
-  if (typeof value === "function") {
-    return console.log("function");
-  }
-  if (typeof value === "symbol") {
-    return console.log("symbol");
-  }
-  if (typeof value === "bigint") {
-    return console.log("bigint");
-  }
-};
-
-getTypeFromValue("hello"); // string
-getTypeFromValue(123); // number
-getTypeFromValue(true); // boolean
-
 const getUserValue = () => {
   const value1 = prompt("Enter value 1");
   const value2 = prompt("Enter value 2");
