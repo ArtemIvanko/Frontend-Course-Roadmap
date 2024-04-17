c7cb287f796426667bff0bdf754c0d96c8e51b4a
ArtemIvanko <tema30k@gmail.com>
2024-04-17 12:10:02 +0300

    Merge remote-tracking branch 'refs/remotes/origin/lesson-3/final' into lesson-3/prompt-data-receiver

--- Lesson 3/script.js
+++ Lesson 3/script.js
@@ -1,3 +1,34 @@
+const getTypeFromValue = (value) => {
+  if (typeof value === "string") {
+    return console.log("string");
+  }
+  if (typeof value === "number") {
+    return console.log("number");
+  }
+  if (typeof value === "boolean") {
+    return console.log("boolean");
+  }
+  if (typeof value === "object") {
+    return console.log("object");
+  }
+  if (typeof value === "undefined") {
+    return console.log("undefined");
+  }
+  if (typeof value === "function") {
+    return console.log("function");
+  }
+  if (typeof value === "symbol") {
+    return console.log("symbol");
+  }
+  if (typeof value === "bigint") {
+    return console.log("bigint");
+  }
+};
+
+getTypeFromValue("hello"); // string
+getTypeFromValue(123); // number
+getTypeFromValue(true); // boolean
+
 const getUserValue = () => {
   const value1 = prompt("Enter value 1");
   const value2 = prompt("Enter value 2");
@@ -9,3 +40,5 @@ const getUserValue = () => {
 };
 
 getUserValue();
+
+//TODO: Refactor the code. Create separate functions for each task.
