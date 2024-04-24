3cc916929c86229a0d6aeb66c0f496a64d71bc4c
ArtemIvanko <tema30k@gmail.com>
2024-04-24 12:46:17 +0300

    Created and used isPrime

--- main/lesson5.js
+++ main/lesson5.js
@@ -16,3 +16,30 @@ const getIntegerList = (num) => {
 };
 
 getIntegerList(num);
+
+//Возможно я с верхней частью немного не так сделал, поэтому вот второй варик, который скорее всего и есть правильный
+
+let n = +prompt("Enter number");
+let isPrime = true;
+
+const showPrimes = (n) => {
+  if (n > 1) {
+    for (let i = 2; i < n; i++) {
+      if (n % i === 0) {
+        isPrime = false;
+        
+        break;
+      }
+    }
+
+    if (isPrime) {
+      console.log("Number is prime");
+    } else {
+      console.log("Number is not prime");
+    }
+  }
+
+  return isPrime;
+};
+
+showPrimes(n);
