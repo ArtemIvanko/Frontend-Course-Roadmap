c3f2353b693ec98cbc4784eeaa825beccac3dd64
ArtemIvanko <tema30k@gmail.com>
2024-04-24 12:14:41 +0300

    Added condition if there is no value set

--- main/lesson5.js
+++ main/lesson5.js
@@ -1,6 +1,11 @@
 let num = +prompt("Enter number number");
 
 for (let i = 1; i <= 100; i++) {
+  if (!num || num <= 0) {
+    console.log("Invalid input data");
+    break;
+  }
+
   if (i * i <= num) {
     console.log(i);
   }
