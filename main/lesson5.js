ca6671ba1c4ef6edc267eb5549b8c9b15e209837
ArtemIvanko <tema30k@gmail.com>
2024-04-24 11:58:06 +0300

    Created and used loop stepper

---
+++ main/lesson5.js
@@ -0,0 +1,8 @@
+let stepNumbers = "";
+
+for (let i = 20; i <= 30; i += 0.5) {
+  console.log(i); //Each step in the loop will be printed to the console
+  stepNumbers += i + " ";
+}
+
+console.log(stepNumbers); //Or in one line separated by a space
