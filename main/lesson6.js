3d7c863dff32a0add43b4bf36f84020f67e18555
ArtemIvanko <tema30k@gmail.com>
2024-04-27 11:08:59 +0300

    Created and used getAverage

---
+++ main/lesson6.js
@@ -0,0 +1,13 @@
+const arr = [1, 2, 3, 4, 5, "hello", "world", "js", false, true, null, undefined, 110];
+const filteredArr = arr.filter((item) => typeof item === "number");
+let sum = 0;
+
+const getAverage = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+
+  return sum / arr.length;
+};
+
+getAverage(filteredArr);
