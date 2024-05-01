7564dd0b5313649df247cd4f6a68a30f60bc6487
ArtemIvanko <tema30k@gmail.com>
2024-05-01 20:27:17 +0300

    Added condition if type of value is not a number

--- main/lesson7.js
+++ main/lesson7.js
@@ -2,6 +2,11 @@ const runLoop = () => {
   for (let i = 0; i < 10; i++) {
     const value = +prompt("Enter a number");
 
+    if (typeof value !== "number" || isNaN(value)) {
+      console.log("Invalid value");
+      continue;
+    }
+
     if (value >= 100) {
       console.log(value);
       break;
