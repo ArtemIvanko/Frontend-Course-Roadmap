f0c5e0a1a622a4dbf29ce3734d70a9e5d66dd262
ArtemIvanko <tema30k@gmail.com>
2024-07-23 17:35:22 +0300

    Fixed imports usage

--- main/server.js
+++ main/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
+const todoRoutes = require("./routes/todos");
 
 const app = express();
 
@@ -17,7 +18,7 @@ mongoose.connect("mongodb://localhost:27017/todoapp", {
 .then(() => console.log("MongoDB connected..."))
 .catch(err => console.error(err));
 
-app.use("/api/todos", require("./routes/todos"));
+app.use("/api/todos", todoRoutes);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
