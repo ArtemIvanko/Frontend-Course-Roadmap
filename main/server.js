c7e1b6da560804c3ccf26b4cfc4c441f3cdd4375
ArtemIvanko <tema30k@gmail.com>
2024-07-23 14:48:23 +0300

    Added server.js

---
+++ main/server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors()); // Enable CORS
+
+app.use(express.static(path.join(__dirname, "public")));
+
+mongoose.connect("mongodb://localhost:27017/todoapp", {
+  useNewUrlParser : true,
+  useUnifiedTopology : true,
+})
+.then(() => console.log("MongoDB connected..."))
+.catch(err => console.error(err));
+
+app.use("/api/todos", require("./routes/todos"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
