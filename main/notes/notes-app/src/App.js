d1f9c96aeedfae0c4844f612b0c6eecab6768047
ArtemIvanko <tema30k@gmail.com>
2024-07-27 15:08:01 +0300

    React notes app init

--- main/notes/notes-app/src/App.js
+++ main/notes/notes-app/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { createContext } from "react";
+import { Container } from "./Container";
+import { BrowserRouter } from "react-router-dom";
+
+export const Context = createContext();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Context.Provider value={{}}>
+        <Container/>
+      </Context.Provider>
+    </BrowserRouter>
   );
 }
 
