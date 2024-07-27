572e43f1bd8f3726778ea65cc83d527fb054bb7b
ArtemIvanko <tema30k@gmail.com>
2024-07-27 15:58:22 +0300

    voteFunctionCounter.jsx created and used

--- main/lesson25-26/src/App.js
+++ main/lesson25-26/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { VoteClassCounter } from "./voteClassCounter";
+import { VoteFunctionCounter } from "./voteFunctionCounter";
 
 function App() {
   return (
     <div className="App">
       <VoteClassCounter/>
+      <VoteFunctionCounter/>
     </div>
   );
 }
