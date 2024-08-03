18449dae51c2570978ce5fea62680d4454c7ff71
ArtemIvanko <tema30k@gmail.com>
2024-08-03 12:59:02 +0300

    Created and used Contacts page

---
+++ main/lesson-27/src/main/Contacts/Contacts.jsx
@@ -0,0 +1,11 @@
+export const Contacts = () => {
+  return (
+    <div>
+      <h2>Contacts</h2>
+      <p>
+        Email: <span>something@gmail.com</span>
+        Phone: <span>1234567890</span>
+      </p>
+    </div>
+  );
+};
