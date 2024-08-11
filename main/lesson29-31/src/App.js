054c625bee6b7556d249191ab0fd7edaed8a653d
ArtemIvanko <tema30k@gmail.com>
2024-08-11 15:09:25 +0300

    Updated App.js

--- main/lesson29-31/src/App.js
+++ main/lesson29-31/src/App.js
@@ -1,9 +1,10 @@
 import { Provider } from "react-redux";
 import { store } from "./store";
 import { TodoList } from "./TodoList";
+import { SwapiComponent } from "./SwapiComponent";
 
 export const App = () => (
   <Provider store={store}>
-    <TodoList/>
+    <SwapiComponent/>
   </Provider>
 );
