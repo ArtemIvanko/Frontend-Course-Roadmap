66a082e20f9830ede5324bbfb3ec53842de19951
ArtemIvanko <tema30k@gmail.com>
2024-08-22 12:36:16 +0300

    Updated store due to redux-saga

--- main/lesson29-31/src/store.js
+++ main/lesson29-31/src/store.js
@@ -1,12 +1,20 @@
 import { configureStore } from "@reduxjs/toolkit";
+import createSagaMiddleware from "redux-saga";
 import swapiReducer from "./swapiSlice";
 import todosReducer from "./todosSlice";
+import rootSaga from "./sagas";
 import { thunk } from "redux-thunk";
+import { sagaTodosReducer } from "./sagaTodoReducer";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export const store = configureStore({
   reducer : {
     swapi : swapiReducer,
     todos : todosReducer,
+    sagaTodos : sagaTodosReducer,
   },
-  middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
+  middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk, sagaMiddleware),
 });
+
+sagaMiddleware.run(rootSaga);
