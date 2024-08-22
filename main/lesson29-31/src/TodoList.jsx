de2ffd828120536d6fadfe91dce9931d45fdc657
ArtemIvanko <tema30k@gmail.com>
2024-08-22 12:38:14 +0300

    Created and used TodoList

---
+++ main/lesson29-31/src/TodoList.jsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTodosRequest, addTodo, removeTodo } from "./actions";
+
+export const TodoList = () => {
+  const dispatch = useDispatch();
+  const { todos, loading, error } = useSelector((state) => state.sagaTodos);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    dispatch(fetchTodosRequest());
+  }, [dispatch]);
+
+  const handleAddTodo = () => {
+    if (newTodo.trim()) {
+      dispatch(addTodo({ id : Date.now(), title : newTodo }));
+      setNewTodo("");
+    }
+  };
+
+  const handleRemoveTodo = (id) => {
+    dispatch(removeTodo(id));
+  };
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button onClick={handleAddTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.title}
+            <button onClick={() => handleRemoveTodo(todo.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
