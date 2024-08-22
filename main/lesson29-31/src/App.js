516e063f7ceefd51564e6e24b6abd674e957ad1c
ArtemIvanko <tema30k@gmail.com>
2024-08-22 12:51:33 +0300

    Code cleanup

--- main/lesson29-31/src/App.js
+++ main/lesson29-31/src/App.js
@@ -1,6 +1,5 @@
 import { Provider } from "react-redux";
 import { store } from "./store";
-import { TodoList } from "./TodoList";
 import { SwapiComponent } from "./SwapiComponent";
 
 export const App = () => (
